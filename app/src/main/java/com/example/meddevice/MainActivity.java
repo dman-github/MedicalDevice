4ab47cbb4dcfda3957dab772d87eefeeb7542847
Damitha Arachchige <Damitha.Arachchige@sensium.co.uk>
2023-02-09 15:51:49 +0000

    tested write file to app directory.
    parsed the portal user list and compared against current users

--- app/src/main/java/com/example/meddevice/MainActivity.java
+++ app/src/main/java/com/example/meddevice/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.meddevice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.Format;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,18 +18,25 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private Map<String, UserStatus> currentUsers = new HashMap<String, UserStatus>();
-    private int deviceId = -1;
+    private int currentDeviceId = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
             Resources res = getResources();
-            // Read the text file from R.raw resources
-            InputStream inputStream = res.openRawResource(R.raw.deviceuserlist);
+            // Read the text files from R.raw resources
+            InputStream inputStreamDevice = res.openRawResource(R.raw.deviceuserlist);
+            InputStream inputStreamPortal = res.openRawResource(R.raw.portaluserlist);
+            BufferedReader bufferedReaderDevice = new BufferedReader(new InputStreamReader(inputStreamDevice));
+            BufferedReader bufferedReaderPortal = new BufferedReader(new InputStreamReader(inputStreamPortal));
+            String outPutFileName = "OutputFile.txt";
+            FileOutputStream outputStream = openFileOutput(outPutFileName, Context.MODE_PRIVATE);
+
             currentUsers.clear();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line = bufferedReader.readLine();
+
+            /* Read the device user list and create the User objects first */
+            String line = bufferedReaderDevice.readLine();
             while (line != null) {
                 String[] thisLine = line.split("\t");
                 // Each line of data is tab-limited to 3 columns
@@ -39,17 +49,57 @@ public class MainActivity extends AppCompatActivity {
                     User user = new User(uuid,
                             Integer.parseUnsignedInt(deviceId),
                             statusString);
-                    if (currentUsers.containsKey(uuid)) {
-                        System.out.printf("Duplicate uuid %s%n", uuid);
+                    if (currentDeviceId < 0) {
+                        // copy the device Id from the first user. All the users on the
+                        // deviceUserList have the same device.
+                        currentDeviceId = user.getDeviceId();
                     }
-                    // populate dictionary
+                    if (currentUsers.containsKey(uuid)) {
+                        String textToWrite = String.format("Duplicate uuid %s%n", uuid);
+                        System.out.printf(textToWrite);
+                        outputStream.write(textToWrite.getBytes());
+                    }
+                    // populate dictionary of currentUsers
                     currentUsers.put(uuid, user.getUserStatus());
                 }
                 // read next line
-                line = bufferedReader.readLine();
-
+                line = bufferedReaderDevice.readLine();
             }
-            bufferedReader.close();
+            bufferedReaderDevice.close();
+
+            // Read the portalUserList and only update the users that
+            line = bufferedReaderPortal.readLine();
+            while (line != null) {
+                String[] thisLine = line.split("\t");
+                // Each line of data is tab-limited to 3 columns
+                if (thisLine.length == 3)
+                {
+                    String portalUuid = thisLine[0];
+                    String portalDeviceId = thisLine[1];
+                    String statusString = thisLine[2];
+                    // If the deviceId is the same as this system then we update the user data
+                    int deviceIdUnsignedint = Integer.parseUnsignedInt(portalDeviceId);
+                    if (deviceIdUnsignedint == currentDeviceId) {
+                        User user = new User(portalUuid,
+                                deviceIdUnsignedint,
+                                statusString);
+                        // check if the portal uuid is present in the current list of users
+                        if (currentUsers.containsKey(portalUuid)) {
+                            // update the user
+                            System.out.printf(">>>>>>Update portal user %s   %s%n", portalUuid,statusString);
+                            currentUsers.put(portalUuid, user.getUserStatus());
+                        } else {
+                            // New user
+                            System.out.printf("Portal has new user %s%n", portalUuid);
+                        }
+                    }
+                }
+                // read next line
+                line = bufferedReaderPortal.readLine();
+            }
+            bufferedReaderPortal.close();
+
+            outputStream.close();
             System.out.printf("Dictionary size %d%n", currentUsers.size());
             UserStatus s = currentUsers.get("7be519b2-96f6-4b4c-b47b-39c555186ffc");
             System.out.printf("         userIsAuthorised:%b, userIsTrainedOnDevice:%b, userIsAdmin:%b%n",
